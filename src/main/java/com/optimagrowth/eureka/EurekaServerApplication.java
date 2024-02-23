9a94a71343a1ce0ecd3fc9b4cb2b28557d58c8a4
Ilya96s <i.kaltygin@icloud.com>
2024-02-23 15:36:37 +0700

    enable eureka server

--- src/main/java/com/optimagrowth/eureka/EurekaServerApplication.java
+++ src/main/java/com/optimagrowth/eureka/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.optimagrowth.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Разрешает приложению работать в роли реестра слубж Spring
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
